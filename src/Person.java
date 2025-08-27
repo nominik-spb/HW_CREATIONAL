5c71358c03199afe8c014c98811e2c21dfaffd28
nominik-spb <nominik.spb@gmail.com>
2025-08-27 20:32:07 +0300

    initial

---
+++ src/Person.java
@@ -0,0 +1,46 @@
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected OptionalInt age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = OptionalInt.of(age);
+    }
+
+    public boolean hasAge() { return age.isPresent();}
+    public boolean hasAddress() { return !address.isEmpty();}
+
+    public String getName() {return name;}
+    public String getSurname() {return surname;}
+    public OptionalInt getAge() { return age;}
+    public String getAddress() { return address;}
+
+    public void setAddress(String address) { this.address = address; }
+    public void happyBirthday() {
+        if (age.isPresent()) {
+            age = OptionalInt.of(age.getAsInt()+1);
+        }
+    }
+
+    @Override
+    public String toString() {return getName();}
+
+    @Override
+    public int hashCode() {return 1; }
+
+    public PersonBuilder newChildBuilder() {return new PersonBuilder()
+            //.setName(String name)
+            .setSurname(this.surname)
+            .setAge(this.age.getAsInt())
+            .setAddress(this.address);}
+}
---
+++ src/PersonBuilder.java
@@ -0,0 +1,40 @@
+import java.util.OptionalInt;
+
+public class PersonBuilder {
+
+    public String name;
+    public String surname;
+    public int age;
+    public String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        if (!name.isEmpty() && !surname.isEmpty()) {
+            if (age<0) {
+                throw new IllegalStateException("Неверное значение поля Возраст");
+            }
+            return new Person(this.name, this.surname, this.age);
+        }  else {
+            throw new IllegalStateException("Не заполнены обязательные поля");
+        }
+    }
+}
